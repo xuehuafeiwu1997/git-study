92820202c1e8721aaef0d03481fa1ca332923a20
xumingyang <xumingyang@greatyun.com>
2021-05-20 17:47:12 +0800

    第一个java程序

---
+++ src/java/demo01.java
@@ -0,0 +1,11 @@
+package java;
+
+/**
+ * @author xmy
+ * @date 2021/5/20 5:45 下午
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("第一个java程序");
+    }
+}
---
+++ src/java/demo02.java
@@ -0,0 +1,5 @@
+package java;/**
+ * @author  xmy
+ * @date  2021/5/20 5:46 下午
+ */public class demo02 {
+}
